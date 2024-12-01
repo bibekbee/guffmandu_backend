f57d3202fd0560ece6efe6c0eb465e3a1e8040ed
bibekbee <bibekbee23@gmail.com>
2024-12-01 17:42:28 +0545

    changes

--- GuffMandu/deployment_settings.py
+++ GuffMandu/deployment_settings.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -37,7 +37,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default = os.environ.get['DATABASES_URL'],
+        default = os.environ.get('DATABASES_URL'),
         conn_max_age = 600
     )
 }
