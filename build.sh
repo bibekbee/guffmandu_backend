e36d901757c020d844a8ac2ff6bf2bd4605f84d5
bibekbee <bibekbee23@gmail.com>
2024-12-01 18:49:12 +0545

    changes

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+python manage.py makemigrations accounts
+python manage.py makemigrations economy
+python manage.py makemigrations feedback_and_report
+
 python manage.py migrate
